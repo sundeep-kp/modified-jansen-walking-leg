74c87dd62c8ff1f5a5ef52c29e57e176484daaaa
sundeep-kp <sundeep25101@iiitnr.edu.in>
2026-09-14 00:24:26 +0530

    linkage complete, shoe object added

--- template_mujoco.py
+++ template_mujoco.py
@@ -1,15 +1,15 @@
 import mujoco
 import mujoco.viewer
 import time
+from pathlib import Path
 
-model = mujoco.MjModel.from_xml_path("modified_jansen_leg.xml")
+xml_path = Path(__file__).parent / "modified_jansen_leg.xml"
+
+model = mujoco.MjModel.from_xml_path(str(xml_path))
 data = mujoco.MjData(model)
 
 with mujoco.viewer.launch_passive(model, data) as viewer:
     while viewer.is_running():
-
         mujoco.mj_step(model, data)
-
         viewer.sync()
-
         time.sleep(model.opt.timestep)
